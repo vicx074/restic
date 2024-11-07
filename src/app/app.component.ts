e1d4c89f6b4ca7ce331f026cda7fa080f269b684
vicx074 <victororg22@gmail.com>
2024-11-06 22:52:53 -0300

    primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, DoCheck{
+  title = 'app-lista-de-compras';
+  listaDeCompra! : Array<Item>;
+  itemParaSerEditado!: Item;
+  constructor(private listaService: ListaDeCompraService) { }
+  ngOnInit(): void {
+    this.listaDeCompra = this.listaService.getListaDeCompra();
+    console.log(this.listaDeCompra);
+  }
+
+  editarItem(item: Item){
+    this.itemParaSerEditado = item;
+   }
+
+   deletarItem(id: Number){
+    const index = this.listaDeCompra.findIndex(item => item.id === id);
+    this.listaDeCompra.splice(index, 1);
+   }
+
+   limparLista(){
+    this.listaDeCompra = [];
+   
+   }
+
+   ngDoCheck() {
+    console.log('DoCheck foi chamado');
+    this.listaService.atualizarLocalStorage();
+
+   }
+
+}
---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,48 @@
+import { outputAst } from '@angular/compiler';
+import { Component, Input, OnChanges, OnDestroy, OnInit, Output } from '@angular/core';
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
+import { EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() item!: Item;
+  @Output() emitindoItemParaEditar = new EventEmitter( );
+  @Output() emitindoIdParaDeletar = new EventEmitter( );
+
+  faPen = faPen;
+  faTrash = faTrash
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  ngOnChanges(): void { }
+
+  editarItem(){
+    this.emitindoItemParaEditar.emit(this.item);
+
+  }
+
+  checarItem() {
+    if(this.item.comprado == true){
+      this.item.comprado = false;
+    }else{
+      this.item.comprado = true;
+    }
+  }
+    deletarItem(){
+      console.log("Estão tentando me calar.");
+      this.emitindoIdParaDeletar.emit(this.item.id);
+    }
+
+    ngOnDestroy(){
+      console.log("Componente destruido");
+
+    }
+
+}
